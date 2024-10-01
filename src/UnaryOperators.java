6fee1df58c5a2e4b37747f47791a2296008fb3cf
gauravkarunesh88@gmail.com <gauravkarunesh88@gmail.com>
2024-10-01 17:20:03 +0530

    operators

--- src/UnaryOperators.java
+++ src/UnaryOperators.java
@@ -1,6 +1,12 @@
 public class UnaryOperators {
     public static void main (String[] args) {
         int a=10;
+        int b=20;
         System.out.println(a++);
+        System.out.println(++a);
+
+        System.out.println(b--);
+        System.out.println(--b);
+
     }
 }
