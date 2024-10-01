a054584a6366b497af978a7c334673d797ef651c
gauravkarunesh88@gmail.com <gauravkarunesh88@gmail.com>
2024-10-01 16:40:21 +0530

    operators

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperators {
+    public static void main (String[] args) {
+        int a= 10;
+        int b=50;
+
+
+        String num1= "15";
+        String num2 = "25";
+
+        int a1= Integer.parseInt(num1);
+        int a2= Integer.parseInt(num2);
+
+        System.out.println("a+b="  +(a+b));
+    }
+}
---
+++ src/TestJava.java
@@ -0,0 +1,5 @@
+public class TestJava {
+    public static void main(String[] as){
+        System.out.println("Hello Java");
+    }
+}
