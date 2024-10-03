134afc9c27de8b046fecd5219cca6cbf75170629
gauravkarunesh88@gmail.com <gauravkarunesh88@gmail.com>
2024-10-03 12:22:53 +0530

    operators

--- src/UnaryOperators.java
+++ src/UnaryOperators.java
@@ -8,5 +8,18 @@ public class UnaryOperators {
         System.out.println(b--);
         System.out.println(--b);
 
+        //turnary operator
+        int x= 70, y=30, z=50, result;
+
+        result= ((x > y) ? (x > z) ? x : z : (y > z) ? y : z);
+        System.out.println("Max of three numbers : "+result);
+
+
+        for(int i=0; i<10; i++){
+            if(i%2!=0){
+                continue;
+            }
+            System.out.println(i+" ");
+        }
     }
 }
